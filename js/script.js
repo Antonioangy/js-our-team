49227ee1062e4ab878fb2526e7d109cd9213d98c
Antonioangy <dumitruangy26@gmail.com>
2021-11-04 16:25:22 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+// Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
+// array membri
+const utenti = [
+    {
+        image: 
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+    },
+    
+    {
+        image: ;
+        name: "Angela Caroll",
+        role: "Chief Editor",
+    } ,
+    {
+        image: ;
+        name: "Walter Gordon",
+        role: "Office Manager",
+    },
+    {
+        image: ;
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+    },
+    {
+        image: ;
+        name: "Scott Estrada",
+        role: "Developer",
+    },
+    {
+        image: ;
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+    }
+]
+
+console.log(utenti)
+
+let image = document.getElementById("image").value
+let name = document.getElementById("name").value
+let role = document.getElementById("role").value
+console.log(name)
+
+
